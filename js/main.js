6ed932eeadca7f55e43ffdeff082343516fa7e6e
jaison10 <jaisonj1010@gmail.com>
2021-03-07 20:05:15 +0530

    checkout page

--- js/main.js
+++ js/main.js
@@ -194,4 +194,45 @@
 
     
 
+
+
+    $('.js-show-header-dropdown').on('click',function(){
+        $(this).parent().find('.header-dropdown')
+    });
+    var menu=$('. ');
+    var sub_menu_is_showed=-1;
+    for(var i=0;i<menu.length;i++){
+        $(menu[i]).on('click',function(){
+        if(jQuery.inArray(this,menu)==sub_menu_is_showed)
+        {
+            $(this).parent().find('.header-dropdown').toggleClass('show-header-dropdown');
+            sub_menu_is_showed=-1;
+        }
+        else{
+            for(var i=0;i<menu.length;i++)
+            {
+                $(menu[i]).parent().find('.header-dropdown').removeClass("show-header-dropdown");
+            }
+        $(this).parent().find('.header-dropdown').toggleClass('show-header-dropdown');
+        sub_menu_is_showed=jQuery.inArray(this,menu);
+    }
+});
+}
+$(".js-show-header-dropdown, .header-dropdown").click(
+function(event){
+    event.stopPrpoagation();
+}
+);
+$(window).on("click",function(){
+for(var i=0;i<menu.length;i++){
+    $(menu[i]).parent().find('.header-dropdown').removeClass("show-header-dropdown");
+}
+sub_menu_is_showed=-1;
+});
+
+
+
+
+
+
 })(jQuery);
